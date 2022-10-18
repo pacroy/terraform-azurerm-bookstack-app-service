387de5a0b892367b3b12fc483c789b79adf415e3
Par <pacroy@users.noreply.github.com>
2022-10-18 21:02:23 +0700

    Update ssl_minimal_tls_version_enforced

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "azurerm_mysql_server" "main" {
   infrastructure_encryption_enabled = false
   public_network_access_enabled     = true
   ssl_enforcement_enabled           = false
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
+  ssl_minimal_tls_version_enforced  = "TLSEnforcementDisabled"
 
   tags = {}
 }
