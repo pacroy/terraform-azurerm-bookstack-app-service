ba0c8b0e89c04eb8a9b5727c37c328eb92f806bf
Par <pacroy@users.noreply.github.com>
2022-10-18 20:03:25 +0700

    Add missing http://

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "azurerm_linux_web_app" "main" {
   service_plan_id     = azurerm_service_plan.main.id
   https_only          = false
   app_settings = {
-    APP_URL     = "${module.naming.app_service.name}.azurewebsites.net"
+    APP_URL     = "http://${module.naming.app_service.name}.azurewebsites.net"
     DB_HOST     = "${azurerm_mysql_server.main.fqdn}:3306"
     DB_USER     = azurerm_mysql_server.main.administrator_login
     DB_PASS     = azurerm_mysql_server.main.administrator_login_password
